dc6053933056b4bef33c4409fc031ef74611328b
SlovA136 <31593971+smurf136@users.noreply.github.com>
2019-11-11 15:08:25 +0700

    Initial commit

---
+++ src/scripts/game.js
@@ -0,0 +1,31 @@
+import 'phaser'
+import '@babel/polyfill'
+
+import MainScene from './scenes/mainScene'
+import PreloadScene from './scenes/preloadScene'
+
+const DEFAULT_WIDTH = 1280
+const DEFAULT_HEIGHT = 720
+
+const config = {
+  backgroundColor: '#ffffff',
+  scale: {
+    parent: 'phaser-game',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT
+  },
+  scene: [PreloadScene, MainScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: { y: 400 }
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  let game = new Phaser.Game(config)
+})
